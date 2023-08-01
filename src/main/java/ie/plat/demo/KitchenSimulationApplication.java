463b3b44834169f7bfdf591f0d63fdd1b12699b4
mshannon-platoon <platoon.development@gmail.com>
2023-08-01 17:10:26 +0100

    Initial commit

---
+++ src/main/java/ie/plat/demo/KitchenSimulationApplication.java
@@ -0,0 +1,13 @@
+package ie.plat.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenSimulationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KitchenSimulationApplication.class, args);
+  }
+
+}
---
+++ src/test/java/ie/plat/demo/KitchenSimulationApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.plat.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenSimulationApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
